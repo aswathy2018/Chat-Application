517ad0a95d6250b24dfc92761b9caa3041112cdf
aswathy2018 <aswathysudhakaran3002@gmail.com>
2026-02-20 19:56:33 +0530

    Initial project setup with basic architecture, index file, and models

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express"
+import { Server } from "socket.io"
+import dotenv from "dotenv"
+import http from 'http'
+import mongoose from "mongoose"
+import cors from "cors"
+
+const app = express()
+dotenv.config()
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+const PORT = process.env.PORT || 5005
+
+//Middleware
+app.use(cors({origin: "true", credentials: true}))
+app.use(express.json())
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("DB connection established.."))
+  .catch((error) => console.log("Error in connecting mongoDB:", error));
+
+console.log("Mongo URI:", process.env.MONGODB_URI);
+
+io.on('connection', (socket)=>{
+    console.log('A user connected');
+})
+
+server.listen(PORT, ()=>console.log(`Server is connected on Port ${PORT}`))
---
+++ backend/models/Message.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    conversation: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Conversation'
+    },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+export default mongoose.model('Message', messageSchema)
