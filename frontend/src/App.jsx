077eac1b7b5cbce9b4b3a9063305d6c4bd821ab7
aswathy2018 <aswathysudhakaran3002@gmail.com>
2026-05-08 19:27:52 +0530

    Route setting and components are created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import { BrowserRouter, Navigate, Route, Router, Routes } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+//Components and pages
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Chat from './components/Chat'
+import Components from './components/Components'
+import CreateConversation from './components/CreateConversation'
 
+export default function App() {
+  const [token, setToken] = useState(localStorage.getItem('token') || '')
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        {
+          !token?(
+            <>
+            <Route path='/signup' element={<Signup/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='*' element={<Navigate to='/login'/>}/>
+            </>
+          ):(
+            <Route path='/create-conversation' element={<CreateConversation/>}/>
+          )
+        }
+      </Routes>
+    </Router>
   )
 }
-
-export default App
---
+++ frontend/src/components/CreateConversation.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreateConversation() {
+  return (
+    <div>CreateConversation</div>
+  )
+}
